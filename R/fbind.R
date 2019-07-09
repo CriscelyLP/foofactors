bf9854c65b760a4f8b9a0a7ab18d0542e21d25d0
CriscelyLP <criscelylujan@gmail.com>
2019-07-09 10:04:43 +0200

    Including documentation

--- R/fbind.R
+++ R/fbind.R
@@ -1,4 +1,16 @@
 
+#' Bind two factors
+#' @description Create a new blabla.
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' a = factor(c("character", "hits", "your", "eyeballs"))
+#' b = factor(c("but", "integer", "where it", "counts"))
+#' fbind(a,b)
 fbind = function(a, b){
 
   x = factor(c(as.character(a), as.character(b)))
